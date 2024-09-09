617785ab3f38e76598f28a790569bc78a61c5bfe
Gabriel Filipe <gabrielpondian@hotmail.com>
2024-09-09 15:21:50 -0300

    Doc: documentação do saque e deposito

--- modules.py
+++ modules.py
@@ -1,18 +1,18 @@
-import datetime
-
+import datetime  # import das bibliotecas
+# declaração dos campos dos saques diarios
 saques_dia = 0
 data_ultimo_saque = datetime.date.today()
 hoje = datetime.date.today()
 
 saldo = ...
-
+# declaração dos campos saldo e valor
 valor = ...
 
 
-def deposito():
+def deposito():  # função para deposito
     global saldo, valor
 
-    if valor > 0 and saldo >= 0:
+    if valor > 0 and saldo >= 0:  # condicional que soma valor com saldo e retorna float com 2 casas decimais
         saldo += valor
         return round(float(saldo), 2)
     
@@ -20,7 +20,7 @@ def deposito():
        return "valor invalido"
     
 
-def saque():
+def saque():  # função para saque
     global saldo, valor, saques_dia, data_ultimo_saque, hoje
     while saques_dia < 4:
         if hoje != data_ultimo_saque:  # verifica o dia do ultimo saque e caso seja outro dia, zera o contador
@@ -30,9 +30,9 @@ def saque():
         if saques_dia >= 3:
             return "Número máximo de saques por dia atingido!"
 
-        elif saldo >= 0 and valor > 0 and valor < saldo and valor <= 500:
+        elif saldo >= 0 and valor > 0 and valor < saldo and valor <= 500:  # condicional que subtrai o valor do saldo
             saldo -= valor
-            saques_dia += 1
+            saques_dia += 1  #  contador de saques diarios
             return round(float(saldo), 2)
 
         elif valor > saldo:
